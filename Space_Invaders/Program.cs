a63e8cd155412a298b620f3e9ef57c5f03c33824
sara <saraala7@gmail.com>
2025-04-06 18:44:28 +0200

    .

---
+++ Space_Invaders/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Space_Invaders;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+    }
+}
